1b709f00976c3fb3ca19b6d66c8dbf7ba67e92e1
elinteerie <elinteerie@gmail.com>
2023-08-18 00:31:19 +0100

    added field first and last_name to serualizer
    added field first and last_name to serualizer

--- clearance/student/serializers.py
+++ clearance/student/serializers.py
@@ -1,10 +1,35 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from account.models import StudentUser, DefaultUser
 
 
+User = get_user_model()
+
+class DefaultUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
+
+
 class StudentProfileRetrieveUpdateSerializer(serializers.ModelSerializer):
+    student = DefaultUserSerializer()
     class Meta:
         model = StudentUser
-        exclude = ['ict_cleared', 'dept_cleared', 'student']
+        exclude = ['ict_cleared', 'dept_cleared']
+        
+    def update(self, instance, validated_data):
+        # Update the associated DefaultUser fields (first_name and last_name)
+        user_data = validated_data.pop('student', {})
+        user = instance.student
+        for attr, value in user_data.items():
+            setattr(user, attr, value)
+        user.save()
+
+        # Update the other fields of the StudentUser instance
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        return instance
 
 
